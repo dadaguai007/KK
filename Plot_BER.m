14388084a5316d092976e96d350ec9131aaadfb3
dadaguai007 <522462231@qq.com>
2025-04-20 18:49:49 +0800

    BER Plot

---
+++ Plot_BER.m
@@ -0,0 +1,20 @@
+addpath('Output\');
+addpath('Plot\')
+load('BER_Dither_0_2_5_8_10.mat')
+load('BER_Algriom_Dither_decode_Group_0_2_5_8_10.mat')
+load('BER_Algriom_Dither_decode_one_by_one_0_2_5_8_10.mat')
+EbNo=15:30;
+berplot = BERPlot_David();
+% 间隔
+berplot.interval=3;
+% 字号
+berplot.Config.FontSize = 14;
+berplot.flagThreshold=1;
+berplot.flagRedraw=0;
+berplot.flagAddLegend=1;
+
+BER=[ber_total(1,:);...
+    ber_total(2,:);ber_total_iter(2,:);ber_total_iter_Group(2,:);];
+LengendArrary=["80km w/o 0%Vpi",...
+    "80km w/o 2%Vpi","80km w SIC Total 2%Vpi","80km w SIC Group 2%Vpi",];
+berplot.multiplot(EbNo,BER,LengendArrary);
